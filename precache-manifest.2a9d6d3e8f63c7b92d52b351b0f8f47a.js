d123815892d5100be42aa7917561e5824c5bae02
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2020-01-26 22:02:36 +0100

    Updates

---
+++ precache-manifest.2a9d6d3e8f63c7b92d52b351b0f8f47a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de723d5a27728fa424e39d5a35ec150f",
+    "url": "/salvare/index.html"
+  },
+  {
+    "revision": "1a92cc166041fdd803e7",
+    "url": "/salvare/static/css/main.3fa35938.chunk.css"
+  },
+  {
+    "revision": "751aa8ea4e4151fdcef1",
+    "url": "/salvare/static/js/2.ae4ac5ad.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/salvare/static/js/2.ae4ac5ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a92cc166041fdd803e7",
+    "url": "/salvare/static/js/main.549aceed.chunk.js"
+  },
+  {
+    "revision": "2762ed8306f890e6f32c",
+    "url": "/salvare/static/js/runtime-main.bedd1661.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/salvare/static/media/logo.5d5d9eef.svg"
+  }
+]);
