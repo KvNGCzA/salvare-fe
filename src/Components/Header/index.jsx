956d72b42a05ce6bc41750dd17f6c64d03d81e15
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2020-01-27 22:29:19 +0100

    build slide out menu

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,11 +3,22 @@ import { Link, withRouter } from "react-router-dom";
 import './index.scss';
 import logo from '../../images/logo.png';
 import menu from '../../images/menu.svg';
+import closeMenu from '../../images/close-menu.svg';
 
 class Header extends Component {
-  showSidebar = () => {
-
+  state = {
+    sidebarLeft: '-100vw'
   }
+
+  showSidebar = () => {
+    const { sidebarLeft } = this.state;
+    if (sidebarLeft === '-100vw') {
+      return this.setState({  sidebarLeft: 0 })
+    }
+    return this.setState({  sidebarLeft: '-100vw' })
+  }
+
+  resetSidebar = () => this.setState({  sidebarLeft: '-100vw' })
 
   render() {
     const { location: { pathname } } = this.props;
@@ -43,13 +54,45 @@ class Header extends Component {
           </nav>
         </div>
         <div className="header mobile-header">
-          <img src={menu} alt="salvare menu-icon" className="menu-icon" />
+          <img src={menu} alt="salvare menu-icon" className="menu-icon" onClick={this.showSidebar}/>
           <Link to="/">
             <img src={logo} alt="salvare logo" className="logo"/>
           </Link>
           <Link to="/profile">
             <div className="profile-image"/>
           </Link>
+          <div className="side-bar" style={{ left: this.state.sidebarLeft }}>
+            <nav>
+              <img src={closeMenu} alt="salvare closemenu-icon" className="menu-icon menu-icon--close" onClick={this.showSidebar}/>
+              <ul>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/' className={pathname === '/' ? 'active' : ''}>home</Link>
+                </li>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/report-a-case' className={pathname === '/report-a-case' ? 'active' : ''}>report a case</Link>
+                </li>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/solve-a-case' className={pathname === '/solve-a-case' ? 'active' : ''}>solve a case</Link>
+                </li>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/donate' className={pathname === '/donate' ? 'active' : ''}>donate</Link>
+                </li>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/about' className={pathname === '/about' ? 'active' : ''}>about us</Link>
+                </li>
+              </ul>
+            </nav>
+            <nav>
+              <ul>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/' className={pathname === '/login' ? 'active' : ''}>login</Link>
+                </li>
+                <li onClick={this.resetSidebar}>
+                  <Link to='/' className={pathname === '/signup' ? 'active' : ''}>signup</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </Fragment>
     );
